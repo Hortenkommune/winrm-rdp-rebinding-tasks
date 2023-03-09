06b4d66be8b29fb1b1546443ded002e1f451d383
2271hatlen <46486198+2271hatlen@users.noreply.github.com>
2023-03-09 17:50:09 +0100

    Added scripts that are triggered

---
+++ RenewWinRmHttps.ps1
@@ -0,0 +1,35 @@
+﻿function Configure-WinRMHttpsListener
+{
+    param
+    (
+        [string] $TemplateName = "Intern-ServerCert"
+    )
+
+    # Get certificate thumbprint based on templatename.
+    $TemplatecertThumbprint = Get-ChildItem 'Cert:\LocalMachine\My' | Where-Object {
+        $_.Extensions | Where-Object {
+            ($_.Oid.FriendlyName -eq 'Certificate Template Information') -and ($_.Format(0) -match $TemplateName)
+        }
+    } | Select-Object -ExpandProperty Thumbprint
+
+    # Get existing winrm thumbprint
+    $ExistingWinRMCertThumbprint = Get-WSManInstance winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} | Select-Object -ExpandProperty CertificateThumbprint
+
+    if ($TemplatecertThumbprint -eq $ExistingWinRMCertThumbprint) {
+        return
+    }
+
+    # Delete existing HTTPS listener if it exists
+    Remove-WSManInstance -ResourceURI winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} -ErrorAction SilentlyContinue
+
+
+    # Get fully qualified hostname
+    $hostname = [System.Net.Dns]::GetHostByName($env:computerName).HostName
+
+    # Configure WinRM
+    New-WSManInstance -ResourceURI winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} -ValueSet @{Hostname=$hostname;CertificateThumbprint=$TemplatecertThumbprint} -ErrorAction SilentlyContinue
+    Set-WSManInstance -ResourceURI winrm/config -ValueSet @{MaxEnvelopeSizekb="8192"}
+    Set-WSManInstance -ResourceURI winrm/config/service/auth -ValueSet @{Basic="false";CbtHardeningLevel="Strict"} -ErrorAction SilentlyContinue
+}
+
+Configure-WinRMHttpsListener
---
+++ SetRDPCertificate.ps1
@@ -0,0 +1,22 @@
+﻿# Get the current certificate thumbprint for the RDP-tcp terminal.
+$class = "Win32_TSGeneralSetting"
+$namespace = "root\cimv2\terminalservices"
+$filter = "TerminalName='RDP-tcp'"
+$currentThumbprint = (Get-WmiObject -class $class -Namespace $namespace -Filter $filter).SSLCertificateSHA1Hash
+
+# Get the certificate thumbprint for the Intern-ServerCert certificate template.
+$TemplateName = "Intern-ServerCert"
+$TemplatecertThumbprint = Get-ChildItem 'Cert:\LocalMachine\My' | Where-Object {
+    $_.Extensions | Where-Object {
+        ($_.Oid.FriendlyName -eq 'Certificate Template Information') -and ($_.Format(0) -match $TemplateName)
+    }
+} | Select-Object -ExpandProperty Thumbprint
+
+# Only run the rest of the script if the template thumbprint is different from the current thumbprint.
+if ($TemplatecertThumbprint -ne $currentThumbprint) {
+    # Get the path to the RDP-tcp terminal setting.
+    $path = (Get-WmiObject -class $class -Namespace $namespace -Filter $filter).__path
+
+    # Set the SSLCertificateSHA1Hash property to the new thumbprint.
+    Set-WmiInstance -Path $path -argument @{SSLCertificateSHA1Hash=$TemplatecertThumbprint}
+}
