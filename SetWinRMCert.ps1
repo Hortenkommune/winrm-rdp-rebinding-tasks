a8acc16888d934bdacb0eda368460f605172f4e5
2271hatlen <46486198+2271hatlen@users.noreply.github.com>
2023-03-10 17:05:31 +0100

    Update SetWinRMCert.ps1

--- SetWinRMCert.ps1
+++ SetWinRMCert.ps1
@@ -1,35 +1,25 @@
-﻿function Configure-WinRMHttpsListener
-{
-    param
-    (
-        [string] $TemplateName = "Intern-ServerCert"
-    )
-
-    # Get certificate thumbprint based on templatename.
-    $TemplatecertThumbprint = Get-ChildItem 'Cert:\LocalMachine\My' | Where-Object {
-        $_.Extensions | Where-Object {
-            ($_.Oid.FriendlyName -eq 'Certificate Template Information') -and ($_.Format(0) -match $TemplateName)
-        }
-    } | Select-Object -ExpandProperty Thumbprint
-
-    # Get existing winrm thumbprint
-    $ExistingWinRMCertThumbprint = Get-WSManInstance winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} | Select-Object -ExpandProperty CertificateThumbprint
-
-    if ($TemplatecertThumbprint -eq $ExistingWinRMCertThumbprint) {
-        return
+# Get certificate thumbprint based on templatename.
+$TemplateName = "Intern-ServerCert"
+$TemplatecertThumbprint = Get-ChildItem 'Cert:\LocalMachine\My' | Where-Object {
+    $_.Extensions | Where-Object {
+        ($_.Oid.FriendlyName -eq 'Certificate Template Information') -and ($_.Format(0) -match $TemplateName)
     }
+} | Select-Object -ExpandProperty Thumbprint
 
-    # Delete existing HTTPS listener if it exists
-    Remove-WSManInstance -ResourceURI winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} -ErrorAction SilentlyContinue
+# Get existing winrm thumbprint
+$ExistingWinRMCertThumbprint = Get-WSManInstance winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} | Select-Object -ExpandProperty CertificateThumbprint
 
-
-    # Get fully qualified hostname
-    $hostname = [System.Net.Dns]::GetHostByName($env:computerName).HostName
-
-    # Configure WinRM
-    New-WSManInstance -ResourceURI winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} -ValueSet @{Hostname=$hostname;CertificateThumbprint=$TemplatecertThumbprint} -ErrorAction SilentlyContinue
-    Set-WSManInstance -ResourceURI winrm/config -ValueSet @{MaxEnvelopeSizekb="8192"}
-    Set-WSManInstance -ResourceURI winrm/config/service/auth -ValueSet @{Basic="false";CbtHardeningLevel="Strict"} -ErrorAction SilentlyContinue
+if ($TemplatecertThumbprint -eq $ExistingWinRMCertThumbprint) {
+    return
 }
 
-Configure-WinRMHttpsListener
+# Delete existing HTTPS listener if it exists
+Remove-WSManInstance -ResourceURI winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} -ErrorAction SilentlyContinue
+
+# Get fully qualified hostname
+$hostname = [System.Net.Dns]::GetHostByName($env:computerName).HostName
+
+# Configure WinRM
+New-WSManInstance -ResourceURI winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} -ValueSet @{Hostname=$hostname;CertificateThumbprint=$TemplatecertThumbprint} -ErrorAction SilentlyContinue
+Set-WSManInstance -ResourceURI winrm/config -ValueSet @{MaxEnvelopeSizekb="8192"}
+Set-WSManInstance -ResourceURI winrm/config/service/auth -ValueSet @{Basic="false";CbtHardeningLevel="Strict"} -ErrorAction SilentlyContinue
